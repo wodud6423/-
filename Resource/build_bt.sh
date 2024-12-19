24d2934c7d10f5e892e489343d02b05434c6622e
Yeomjeayoung <wodud6423@kau.kr>
2024-12-19 13:03:01 +0900

    Delete Resource directory

--- Resource/build_bt.sh
+++
@@ -1,3 +0,0 @@
-#sudo apt-get install libbluetooth-dev
-gcc iot_client_bluetooth.c -o iot_client_bluetooth -lbluetooth -pthread
-gcc iot_client_bluetooth2.c -o iot_client_bluetooth2 -lbluetooth -pthread
